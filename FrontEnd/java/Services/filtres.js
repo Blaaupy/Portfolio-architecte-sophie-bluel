2deec73ac4a9b87944b35c5a333b109a155bd116
Baptiste <baptiste.laurent0027@gmail.com>
2025-06-11 10:48:36 +0200

    Edit of index.js and creation of filtres.js cause the filters needed to be created with java.

---
+++ FrontEnd/java/Services/filtres.js
@@ -0,0 +1,43 @@
+import { fetchCategories } from "../GetData.js";
+import { afficherGallery } from "./portfolio.js";
+
+export async function creeFiltre() {
+    // Récupération des informations de /categories.
+    const categories = await fetchCategories();
+    const listeFiltre = document.querySelector('.listeFiltre');
+    listeFiltre.innerHTML = '';
+
+    // Bouton Tous
+    const boutonTous = document.createElement('button');
+    boutonTous.textContent = "Tous";
+    boutonTous.classList.add('btn-tous');
+    boutonTous.addEventListener("click", () => afficherGallery());
+    listeFiltre.appendChild(boutonTous);
+
+    // Génération des autres filtres
+    categories.forEach(category => {
+        const buttonFiltre = document.createElement('button'); 
+        buttonFiltre.textContent = category.name;
+
+        const className = 'btn-' + category.name.replace(/\s+/g, '-');
+        buttonFiltre.classList.add(className);
+
+        buttonFiltre.addEventListener("click", () => {
+            afficherGallery(category.id);
+        });
+
+        listeFiltre.appendChild(buttonFiltre);
+    });
+    console.log(categories);
+    // Ajout de active sur Tous
+    boutonTous.classList.add("active");
+
+    // Gestion de la classe active pour tous les boutons afin de savoir quel est le filtre actif.
+    const boutons = listeFiltre.querySelectorAll('button');
+    boutons.forEach(button => {
+        button.addEventListener('click', () => {
+            boutons.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+        });
+    });
+}
--- FrontEnd/java/index.js
+++ FrontEnd/java/index.js
@@ -1,48 +1,8 @@
 import {afficherGallery} from './Services/portfolio.js';
-import { fetchCategories } from './GetData.js';
+import { creeFiltre } from './Services/filtres.js';
 
-// Filtres
-
-// Bouton Tous qui affiche tous les travaux de Sophie. on remarque que rien n'est précisé dans la fonction
-// afficher gallery, c'est parce que de base tout est afficher si aucun paramètre n'est precisé.
-const btnTous = document.querySelector('.btn-tous');
-
-btnTous.addEventListener("click", async function() { 
+// Affiche au chargement de la page tous les travaux et les filtres.
+window.addEventListener("DOMContentLoaded", async () => {
+    await creeFiltre();
     afficherGallery();
-})
-
-// Ici j'ajoute cet élément pour que lorsque la page est chargée cela clique automatiquement sur le bouton tous afin de tout afficher.
-
-window.addEventListener("DOMContentLoaded", () => {
-  btnTous.click();
-});
-
-// Bouton objet ici on a 1 en paramètre car les objets sont de catégorie 1.
-const btnObjets = document.querySelector('.btn-objets');
-btnObjets.addEventListener("click", async function () {
-    afficherGallery(1);
-})
-
-// Bouton appartements ici on a 2 pour la même raison qu'avec les objets.
-const btnApp  = document.querySelector('.btn-appts');
-btnApp.addEventListener('click', async function () {
-    afficherGallery(2); 
-})
-// Bouton hotels etc...
-const btnHotels = document.querySelector('.btn-hotels');
-btnHotels.addEventListener('click', async function (){
-    afficherGallery(3);
-})
-
-//Ajout d'activité pour l'expérience utilisateur, cela va nous indiquer quel filtre est sélectionné.
-const boutonsFiltre = document.querySelectorAll('.listeFiltre button');
-
-boutonsFiltre.forEach(button => {
-    button.addEventListener('click', () => {
-        // Retirer active de tous les boutons
-        boutonsFiltre.forEach(btn => btn.classList.remove('active'));
-        
-        // Ajouter active au bouton cliqué
-        button.classList.add('active');
-    });
 });
