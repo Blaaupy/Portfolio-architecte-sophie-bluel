e6e2a8437fefadd9fb301a0905cbd16fa84472be
Baptiste <baptiste.laurent0027@gmail.com>
2025-06-19 14:34:55 +0200

    #

--- FrontEnd/JavaScript/Services/modal.js
+++ FrontEnd/JavaScript/Services/modal.js
@@ -1,3 +1,27 @@
+import { fetchWork } from "../GetData.js";
+
+export async function afficherGalleryModal() { /* fonction copier de celle  de portfolio */
+    const galleryModal = document.querySelector(".galleryModal");
+    galleryModal.innerHTML = "";
+    const works = await fetchWork();
+    
+    works.forEach(work => {
+        const figure = document.createElement('figure');
+        figure.classList.add('modal-figure');
+
+        const image = document.createElement('img'); 
+        image.src = work.imageUrl;
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.classList.add('delete-btn');
+        deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+        deleteBtn.setAttribute('data-id', work.id);
+
+        figure.appendChild(image);
+        figure.appendChild(deleteBtn);
+    });
+}
+
 export async function modalFonctionnelle () {
     const btnModifier = document.querySelector('.js-modal');
     const btnClose = document.querySelector('.js-close-modal');
@@ -7,10 +31,16 @@ export async function modalFonctionnelle () {
         modal1.style.display = null;
         modal1.removeAttribute('aria-hidden');
         modal1.setAttribute('ari-modal', 'true');
+        galleryModal();
     })
 
     btnClose.addEventListener("click", () => {/* Ferme la modal en passant l'affichage de null a none */
         modal1.style.display = "none";
+        modal1.removeAttribute('ari-modal', 'true');
+        modal1.setAttribute('aria-hidden');
     })
+    
+    
+    
 
 }
